c77db93d4d0eb474e486e24684a8a3213b4b59ca
Vince van Noort <vince.vannoort@student.hu.nl>
2017-10-04 11:58:12 +0200

    Added inputs for centers

--- Mandelbrot/Mandelbrot.cs
+++ Mandelbrot/Mandelbrot.cs
@@ -8,10 +8,12 @@ namespace Mandelbrot
     {
         // variables for interface
         TextBox inputIterations = new TextBox();
+        TextBox inputCenterX = new TextBox();
+        TextBox inputCenterY = new TextBox();
 
         // variables for mandelbrotview
         Bitmap MandelbrotView;
-		int pixelWidth = 1000, pixelHeight = 1000, margin = 50;
+		int pixelWidth = 1000, pixelHeight = 1000, margin = 50, offset = 30;
         int limit = 20;
         float width, height;
         float xmin, ymin, xmax, ymax;
@@ -24,25 +26,38 @@ namespace Mandelbrot
             inputIterations.BackColor = Color.White;
             inputIterations.Text = "128";
             this.Controls.Add(inputIterations);
+            inputCenterX.Size = new Size(200, 30);
+            inputCenterX.Location = new Point(pixelWidth - inputCenterX.Width - margin, margin - inputCenterX.Height/2 + offset);
+            inputCenterX.BackColor = Color.White;
+            inputCenterX.Text = "0";
+            this.Controls.Add(inputCenterX);
+            inputCenterY.Size = new Size(200, 30);
+            inputCenterY.Location = new Point(pixelWidth - inputCenterY.Width - margin, margin - inputCenterY.Height/2 + offset*2);
+            inputCenterY.BackColor = Color.White;
+            inputCenterY.Text = "0";
+            this.Controls.Add(inputCenterY);
 
             // set form variables
             this.Text = "Mandelbrot";
             this.Size = new Size(this.pixelWidth, this.pixelHeight);
 
-            // initialise values
-            this.width = 2;
-            this.height = 2;
-            this.xmin = -(this.width) / 2;
-            this.ymin = -(this.height) / 2;
-            this.xmax = xmin + width;
-            this.ymax = ymin + height;
-
             // create event handler to draw the mandelbrot
             this.Paint += this.DrawMandelbrot;
+            this.MouseClick += this.ChangeMandelbrotOrigin;
         }
 
 		void DrawMandelbrot(Object obj, PaintEventArgs pea)
 		{
+            // initialise values
+            float centerX = float.Parse(this.inputCenterX.Text);
+            float centerY = float.Parse(this.inputCenterY.Text);
+            this.width = 2;
+            this.height = 2;
+            this.xmin = (-(this.width) / 2);
+            this.ymin = (-(this.height) / 2);
+            this.xmax = (this.xmin + width);
+            this.ymax = (this.ymin + height);
+
             // create bitmap object for view
 			MandelbrotView = new Bitmap(this.pixelWidth, this.pixelHeight);
 
@@ -64,8 +79,8 @@ namespace Mandelbrot
         int CalculateMandelbrot(int i, int j)
         {
             // convert pixels to scale
-            float a = this.MapRange(i, 0, this.pixelWidth, xmin, xmax);
-            float b = this.MapRange(j, 0, this.pixelHeight, ymin, ymax);
+            float a = this.MapRange(i, 0, this.pixelWidth, this.xmin, this.xmax);
+            float b = this.MapRange(j, 0, this.pixelHeight, this.ymin, this.ymax);
 
             // preserve for later
             float x = a;
@@ -95,6 +110,12 @@ namespace Mandelbrot
             }
 
             return (int) MapRange(iteration, 0, iterations, 0, 255);
+        }
+
+        void ChangeMandelbrotOrigin(Object obj, MouseEventArgs mea)
+        {
+            System.Diagnostics.Debug.WriteLine("test!");
+            this.Invalidate();
         }
 
 		// credits to: https://stackoverflow.com/questions/929103/convert-a-number-range-to-another-range-maintaining-ratio
